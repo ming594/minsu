ffa3f1ac32c8e86993defdfd3ec84b64e9abed7a
김민수 <your-email@example.com>
2025-05-19 16:27:48 +0900

    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+// $(function () {
+//     const circle = document.querySelector('.lefttext2 .circle');
+//     const text = document.querySelector('.lefttext2 p');
+
+//     circle.addEventListener('mouseenter', () => {
+//         text.style.color = 'black';
+//     });
+
+//     circle.addEventListener('mouseleave', () => {
+//         text.style.color = 'white';
+//     });
+// })
